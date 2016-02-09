45f24a7bf16d970079dd45d99575c32d45007e8c
Taida-Nirama <taidanirama@gmail.com>
2016-02-09 09:12:17 -0600

    Task1
    Task 1: Creating a new class

---
+++ Television.java
@@ -0,0 +1,14 @@
+// The purpose of this class is to model a television
+// Jessica Gutierrez 20160209
+
+public class Television {
+
+    public static void main(String[] args) {
+      String final MANUFACTURER; //Will hold the brand name of the TV
+      int final SCREEN_SIZE; //Will hold the screen size of the TV
+      int channel; //Will hold the value of the station the TV is showing
+      int volume; //Will hold a number value representing the loudness
+      boolean powerOn; //tells you wether the TV is on (true) or off (false)
+      
+    }
+}
